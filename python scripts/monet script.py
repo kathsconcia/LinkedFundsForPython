53fde3bdaefab065cbc3e52b260ce35bae788459
Greig Bingham <82894092+greigbingham@users.noreply.github.com>
2023-03-08 19:30:49 +0000

    script directly copied from blog (unedited)

---
+++ python scripts/monet script.py
@@ -0,0 +1,50 @@
+import sys
+import clr
+
+# define path to the Mo.net Linked DLL and add this to system path
+
+assembly_path = r"C:\Mo.net Projects\LinkedFundsForPython\Linked_DLL"
+sys.path.append(assembly_path)
+
+# add Mo.net kernel assembly references to the CLR
+
+clr.AddReference("Sal.CalculationEngine.GenCore")
+clr.AddReference("Sal.CalculationEngine.Interfaces.Entities")
+
+# add Mo.net projection DLL reference to the CLR
+
+clr_ref = clr.AddReference("Linked_Monet")
+monet_type = clr_ref.GetType('Monet')
+
+# invoke Mo.net interface methods
+
+initCmdLine_method = monet_type.GetMethod('InitCommandLine')
+initCmdLine_method.Invoke(None, [None])
+
+init_method = monet_type.GetMethod('Initialise')
+init_method.Invoke(None, [True])
+
+initProj_method = monet_type.GetMethod('InitialiseProjections')
+initProj_method.Invoke(None, [])
+
+# obtain a list of the projections inside the DLL
+
+projections = monet_type.GetProperty('Projections')
+
+# create a reference to the Linked projection task
+
+linkedproj = projections.GetValue(None).Linked
+
+# uncomment the next line to override the SterlInt parameter associated with the
+# Linked projection
+
+# linkedproj.SterlInt = 0.1
+
+# run the projection
+
+linkedproj.Run()
+
+# show the sterling result at time=0 
+
+print(f'SterlInt = {linkedproj.SterlInt}')
+print(f'SterlRes(0) = {linkedproj.SterlRes(0)}')
